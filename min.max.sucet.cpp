7c4efe407f7f28fa77330ca3f2d2cbbc2afdcc8f
Toka2k <143963787+Toka2k@users.noreply.github.com>
2024-02-05 23:17:29 +0100

    Update min.max.sucet.cpp
    optimized

--- min.max.sucet.cpp
+++ min.max.sucet.cpp
@@ -11,90 +11,44 @@ int main()
     cin>>cislo2;
     cin>>cislo3;
     spolu = cislo1+cislo2+cislo3;
-    if(cislo1>=cislo2>=cislo3){ //toto je a>b>c, neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo1<<endl;
-        cout<<"Najmensie cislo je: "<<cislo3<<endl;
-        cout<<"Sucet tychto troch cisel je: "<<spolu;
+    
 
+    // POTIALTO to mas dost dobre (aj prvy if si mal dobre len by som dal medzeri aby som to zpriehladnil)
+    // ja by som pokracoval takto
+    // ze zakazdym mas znamienka iba do lava napr. a vymienas len samotne premenne a potom ti nenastane chaos ako v tretej podmienke, ver mi robil som daco taketo len som porovnaval takto 8 premennych
+    // takisto by som odporucil davat takto medzeri aby to bolo priehladnejsie
+    if(cislo1 >= cislo2 >= cislo3){
+        cout<<"Najvacsie cislo je: "<< cislo1 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo3 <<endl;
+        cout<<"Sucet tychto troch cisel je: "<< spolu << endl;
     }
-    else if(cislo1<=cislo2<=cislo3){ //toto je a<b<c, neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo3<<endl;
-        cout<<"Najmensie cislo je: "<<cislo1<<endl;
+    else if(cislo1 >= cislo3 >= cislo2){
+        cout<<"Najvacsie cislo je: "<< cislo1 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo2 <<endl;
+        cout<<"Sucet tychto troch cisel je: "<< spolu <<endl;
+    }
+    else if(cislo2 >= cislo1 >= cislo3){
+        cout<<"Najvacsie cislo je: "<< cislo2 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo3 <<endl;
+        cout<<"Sucet tychto troch cisel je: "<< spolu <<endl;
+    }
+    else if(cislo2 >= cislo3 >= cislo1){
+        cout<<"Najvacsie cislo je: "<< cislo2 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo1 <<endl;
         cout<<"Sucet tychto troch cisel je: "<<spolu<<endl;
-
     }
-    else if((cislo1>=cislo2<=cislo3)&&(cislo1>=cislo3)){ //toto je a>b<c, a>c neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo1<<endl;
-        cout<<"Najmensie cislo je: "<<cislo2<<endl;
+    else if(cislo3 >= cislo1 >= cislo2){
+        cout<<"Najvacsie cislo je: "<< cislo3 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo2 <<endl;
         cout<<"Sucet tychto troch cisel je: "<<spolu<<endl;
-
     }
-    else if((cislo1>=cislo2<=cislo3)&&(cislo1<=cislo3)){ //toto je a>b<c, a<c neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo3<<endl;
-        cout<<"Najmensie cislo je: "<<cislo2<<endl;
-        cout<<"Sucet tychto troch cisel je: "<<spolu<<endl;
-
-    }
-    else if((cislo1<=cislo2>=cislo3)&&(cislo1<=cislo3)){ //toto je a<b>c, a<c neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo2<<endl;
-        cout<<"Najmensie cislo je: "<<cislo1<<endl;
-        cout<<"Sucet tychto troch cisel je: "<<spolu<<endl;
-
-    }
-    else if((cislo1<=cislo2>=cislo3)&&(cislo1>=cislo3)){ //toto je a<b>c, a>c neries iba pomocka pre mna
-        cout<<"Najvacsie cislo je: "<< cislo2<<endl;
-        cout<<"Najmensie cislo je: "<<cislo3<<endl;
+    else if(cislo3 >= cislo2 >= cislo1){
+        cout<<"Najvacsie cislo je: "<< cislo3 <<endl;
+        cout<<"Najmensie cislo je: "<< cislo1 <<endl;
         cout<<"Sucet tychto troch cisel je: "<<spolu<<endl;
 
     }
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//neznasam programovanie
+//milujeme vsetci programovanie
